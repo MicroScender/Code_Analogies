869a322ba0271ada08f6934022bf9ea8101f078e
MicroScender <microcosmic_transcender@protonmail.com>
2022-04-28 19:03:06 -0400

    progressing, (late I might add), towards a 90%+ in 102

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'; 
+
+// declare a variable, assign it the value of the user's input
+// let username= prompt("what is your name?")
+
+// print the user's input to the console
+// console.log(username);
+
+// Checking for Joseph
+
+// if (username === "Joseph") {
+//     console.log("Welcome Sweet Doody!!");
+// } else {
+//     console.log("Welcome! Now GET OUT!!");
+// }
+
+// let feels = prompt("Welcome " + username + "! Feeling blue?");{
+//     console.log("well thats too bad");
+// }
+// How to put data on the page
+// document.write(username);
+
+// username.tooLowerCase() OR username.toUpperCase() to make sure that the username is accepted even if the user types a uppercase for the first letter
+
+// let age = prompt("How old is your cat?");
+// if (age > 20){
+//     console.log ("Wow, that's old!");
+// }
+// else if (age > 10){
+//     console.log("You've had them a while!");
+// }
+// else {
+//     console.log("Aww, kitty!");
+// }  
+
