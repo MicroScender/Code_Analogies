d60b5f65efff2cd1aee1ecb507fa6a40ba7c47bf
MicroScender <microcosmic_transcender@protonmail.com>
2022-04-30 00:20:59 -0400

    Marco and Aaron are lifesavers

--- app.js
+++ app.js
@@ -5,41 +5,46 @@
 
 
 // declare a variable, assign it the value of the user's input
-let surprise= prompt("WOH!! What are you doing here?");
+// let surprise= prompt("WOH!! What are you doing here?");
 
-console.log(surprise);
+// console.log(surprise);
 
-let text;
-if (confirm("Do you really wanna look at this content?") == true) {
-  text = "You pressed yes!";
-} else {
-  text = "You pressed get me outta here!";
+// let text;
+// if (confirm("Do you really wanna look at this content?") == true) {
+//   text = "You pressed yes!";
+// } else {
+//   text = "You pressed get me outta here!";
+// }
+
+// function wassup() {
+// let username= prompt("Mhhmm...Besides the point. What do I call you?");
+// console.log(' Well hello there, ' + username);
+
+// let aimless = prompt ("Are you lost?");
+// let message;
+// if (aimless == 'yes'){
+//     message = 'The Struggler';
+// } else if (aimless == 'no'){
+//     message = 'You have arrived';
+// } else {
+//     message = 'Just a transient';
+// }
+
+// let postage = (username + message);
+// return postage;
+
+// }
+// // call the function with its own name
+// document.write("<p id= 'wassup' ></p>");
+
+let squares = prompt ("How many squares do you want?");
+
+let i = 1; 
+  while (i <= squares) {
+document.write("<image src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAGFBMVEX///8AAAA+Pj47OztCQkJgYGDs7OwNDQ2VKtCdAAABdklEQVR4nO3du20EMRQEQe19pPwzlie/DeoGi6oABmyA/vv6AgAAAAAAAAAA4J6+38/Hguf7+1Dh+1rxPlT4/HTYn+ehwsd1/bw+/UUfr5/repwrfB2aLl5HC09NFwefofCfKNycLhRuThcKN6cLhZvThcLN6ULh5nShcHO6ULg5XSjcnC4Ubk4XCjenC4Wb04XCzelC4eZ0oXBzulC4OV0o3JwuFG5OFwo3pwuFm9OFws3pQuHmdKFwc7pQuDldKNycLhRuThcKN6cLhZvThcLN6ULh5nShcHO6ULg5XSjcnC4Ubk4XCjenC4Wb04XCzelC4eZ0oXBzulC4OV0o3JwuFG5OFwo3pwuFm9OFws3pQuHmdKFwc7pQuDldKNycLhRuThcKN6cLhZvThcLN6ULh5nShcHO6ULg5XSjcnC4Ubk4XCjenC4Wb04XCzelC4eZ0oXBzujhbeP8blje/Q3r/W7L3vwd8/5vOAAAAAAAAAAAAfNovofQM/Reo7UkAAAAASUVORK5CYII=\">");
+i++;
 }
 
-function wassup() {
-let username= prompt("Mhhmm...Besides the point. What do I call you?");
-console.log(' Well hello there, ' + username);
-
-let aimless = prompt ("Are you lost?");
-let message;
-if (aimless == 'yes'){
-    message = 'The Struggler';
-} else if (aimless == 'no'){
-    message = 'You have arrived';
-} else {
-    message = 'Just a transient';
-}
-
-return username;
-}
-
-
-// call the function with its own name
-
-wassup ()
-console.log(wassup())
-
-document.write("<p id='username'>" + username + message + "</p>");
 
 
 // let text;
